edba7c9d634e59c84f281856dc48e2e954865493
MuhammadAyyaz <ayaz319m@gmail.com>
2018-01-20 18:53:33 +0500

    function updated

--- functions.py
+++ functions.py
@@ -5,7 +5,7 @@ def rent_a_car():
     #for choosing car
         car = input('Which car do you want to take in rent?\n')
     #for choosing the sentence to write on car
-        print('select the sentence\n1.Hum chale dushman jale\n2.Dekh kar dua krna\n3.Ye sab meri Maa ki dua hai.')
+        print('select the sentence\n1.Hum chale dushman jale\n2.Dekh kar dua krna\n3.Himmat hai to pass kr warna bardash kr.')
     #selecting the sentence
         msg=int(input('What do you want to write on your car'))
     #asking the user for other car
@@ -17,14 +17,15 @@ def rent_a_car():
             print('Let me see if could find you a', car.title())
     #printing sentence on the car
         if msg == 1:
-            print('Congratulations! we\'ve your car and written'+' Hum chale dushman jale'+' for you on your '+car.title() )
+            print('Congratulations! we\'ve your car and written'+' "Hum chale dushman jale"'+' for you on your '+car.title() )
         elif msg == 2:
-            print('Congratulations! we\'ve your car and written'+' Dekh kar dua krna'+' for you on your'+car.title())
+            print('Congratulations! we\'ve your car and written'+' "Dekh kar dua krna"'+' for you on your '+car.title())
         elif msg == 3:
-            print('Congratulations! we\'ve your car and written'+' Ye sab meri Maa ki dua hai'+' for you on your'+car.title())
+            print('Congratulations! we\'ve your car and written'+' "Himmat hai to pass kr warna bardash kr"'+' for you on your '+car.title())
     #ending condition
         if repeat == 'no':
             car = False
 #Good bye msg
     print('\n ***Hum chale dushman jale***\n   ***Dekh kar dua karna***')
+#calling function
 rent_a_car()
